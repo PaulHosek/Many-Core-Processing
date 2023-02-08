cd2ece022af97af00716c0ae211438dadaad7f9a
GeliezaK <gelieza.gk@gmail.com>
2023-02-08 14:27:57 +0100

    Minor change

--- assignment_1/heat_seq/grid.c
+++ assignment_1/heat_seq/grid.c
@@ -1,4 +1,6 @@
 
+#ifndef GRID_H
+#define GRID_H  
 
 struct pointType {
     double temperature[2];        /* Old and new temperature value */
@@ -20,3 +22,6 @@ struct grid {
 #define WD(g, p)       (g)->points[p].weight_direct             /* Weight as direct neighbor*/
 #define WI(g, p)       (g)->points[p].weight_indirect           /* Weight as diagonal neighbor*/
 #define C(g, p)       (g)->points[p].conductivity               /* Conductivity*/
+
+
+#endif
