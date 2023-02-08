05fd21470d2de5531556b8f4007fa4b703947865
GeliezaK <gelieza.gk@gmail.com>
2023-02-08 13:11:04 +0100

    Add include guards in grid.c

---
+++ test/test_compute.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../assignment_1/heat_seq/grid.c"
+#include "../assignment_1/heat_seq/compute.c"
+
+
+int main(int argc, char **argv)
+{
+    struct parameters p;
+    struct results r;
+
+    read_parameters(&p, argc, argv);
+
+    struct grid grid = initialize(p);
+
+    printf("Get point N+2:  \n", T(&grid, p->N+2));
+
+    return 0;
+}
