cb5bb170bca67f3d3e7105be39eba5ca0ff04231
PMMS course <pmms2304@fs0.cm.cluster>
2023-02-08 09:58:01 +0100

    Write main loop

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -2,15 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include "compute.h"
-
-/// Replace/erase the following line:
 #include "grid.c"
-
-void do_compute(const struct parameters* p, struct results *r)
-{
-/// Replace/erase the following line:
-#include "ref2.c"
-}
 
 double update(int index, struct grid * grid)
 {
@@ -46,3 +38,71 @@ double update(int index, struct grid * grid)
 
     return new_temperature;
 }
+
+
+void do_compute(const struct parameters* p, struct results *r)
+{
+    // Initialize grid 
+    struct grid grid = initialize(p);
+
+    // Measure time
+    struct timespec before, after;
+    clock_gettime(CLOCK_MONOTONIC, &before);
+
+    int it = 0;
+    int converged = 1;
+    do {
+        double maxdiff = 0.0;
+        double tmin = p->io_tmax;
+        double tsum = 0.0;
+        double tmax = p->io_tmin;
+        // Check convergence every timestep
+        int converged = 1;
+
+        for (int index = p->N; index < p->N * (p->M + 1); ++ index){
+            double new_temperature = update(index, &grid);
+
+            double diff = abs(T(&grid, index) - new_temperature);
+
+            // Check all-below-threshold condition
+            if (diff >= p->threshold){
+                converged = 0;
+            }
+
+            if (it % p->period == 0){
+                // Update results 
+                tsum += new_temperature;
+
+                if (diff > maxdiff){
+                    maxdiff = diff;
+                }
+                if (new_temperature > tmax){
+                    tmax = new_temperature;
+                }
+                if (new_temperature < tmin){
+                    tmin = new_temperature;
+                }
+            }
+            
+        }
+
+        // Report results
+        if (it % p->period == 0){
+            r->niter = it;
+            r->tmin = tmin;
+            r->tmax = tmax;
+            r->tavg = tsum/(p->N * p->M);
+            r->maxdiff = maxdiff;
+            clock_gettime(CLOCK_MONOTONIC, &after);
+            r->time = (double)(after.tv_sec - before.tv_sec) +
+              (double)(after.tv_nsec - before.tv_nsec) / 1e9;
+            report_results(p, r);
+        }
+
+        // Flip old and new values
+        grid.old ^= 1;
+
+        ++ it; 
+    } while ((it < p->maxiter) && (!converged));
+}
+
