ee7af20f8942f954004c62782cb9cb4bc605c098
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-02-23 14:27:08 +0100

    - drafted bash

---
+++ assignment_2/vecsort/parallel_v3_both/exp_outer_threads.sh
@@ -0,0 +1,21 @@
+rm exp_outer_thread.csv
+module load prun
+
+directories=(parallel parallel_v1_onlynested parallel_v2_onlyouter parallel_v3_both parallel_v4_inline)
+repetitions=2
+max_threads=16
+
+if [ ! -f exp_outer_thread.csv ]; then
+  touch exp_outer_thread.csv
+  echo "nr_outer_threads, runtime" > exp_outer_thread.csv
+fi
+
+for ((t=1;t<=max_threads;t++)); do
+  make clean
+  make
+  for ((i=1;i<=repetitions;i++)); do
+
+  time=$(prun -np 1 -v -t "$t" parallel_v3_both | grep -oP 'Vecsort took:\s*\K[\d.]+')
+  echo "$dir,$time" >> exp_outer_thread.csv
+  done
+done
