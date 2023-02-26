c06e00465d10f3a145cbea49b314a316e29ebdc9
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-02-26 19:24:45 +0100

    - fixed sequential mergesort: added memcpy

--- assignment_2/vecsort/compare_runtime.sh
+++ assignment_2/vecsort/compare_runtime.sh
@@ -1,7 +1,7 @@
-#rm data.csv
+rm data.csv
 module load prun
 
-directories=(sequential parallel_v1_onlynested parallel_v2_onlyouter parallel_v3_both parallel_v4_inline)
+directories=(sequential parallel_v1_onlynested parallel_v2_onlyouter parallel_v3_both parallel_v4_inline parallel_v5_o3)
 repetitions=100
 
 if [ ! -f data.csv ]; then
@@ -15,7 +15,8 @@ for dir in "${directories[@]}"; do
   make
   for ((i=1;i<=repetitions;i++)); do
 
-  time=$(prun -np 1 -v "$dir" | grep -oP 'Vecsort took:\s*\K[\d.]+')
+#  time=$(prun -np 1 -v "$dir" | grep -oP 'Vecsort took:\s*\K[\d.]+')
+  time=$(prun -np 1 -v "$dir" -x 10000 -l 100000 | grep -oP 'Vecsort took:\s*\K[\d.]+')
   echo "$dir,$time" >> ../data.csv
   done
   cd ..
--- assignment_2/vecsort/parallel_v5_o3/vecsort.c
+++ assignment_2/vecsort/parallel_v5_o3/vecsort.c
@@ -4,29 +4,45 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <ctype.h>
-#include <assert.h>
 #include <omp.h>
+#include <assert.h>
+#include <limits.h>
 
 /* Ordering of the vector */
 typedef enum Ordering {ASCENDING, DESCENDING, RANDOM} Order;
+//short max_threads = (short) omp_get_max_threads();
+short max_threads = 16;
 
 int debug = 0;
-void vecsort(int **vector_vectors, int *vector_lengths, long length_outer);
-void msort(int *v, long l);
+void vecsort(int **vector_vectors, int *vector_lengths, long length_outer, short outer_threads);
+void msort(int *v, long l, short nest_threads);
 void TopDownSplitMerge(long first, long last, int*v);
 
-void vecsort(int **vector_vectors, int *vector_lengths, long length_outer){
-    long i;
-    #pragma parallel for shared(v) firstprivate(i) numthreads(16)
-    for (i =0; i<length_outer; i++){
-        msort(vector_vectors[i], vector_lengths[i]);
+void vecsort(int **vector_vectors, int *vector_lengths, long length_outer, short outer_threads){
+    int nest_threads = max_threads-outer_threads;
+    if (nest_threads < 0 || outer_threads <0){
+        printf("nested_threads = %d, outer_threads = %d nr threads < 0! Exiting...\n",
+               nest_threads,outer_threads );
+        exit(EXIT_FAILURE);
     }
+    long i;
+#pragma omp parallel for schedule(runtime) shared(vector_vectors) num_threads(outer_threads)
+        for (i=0; i<length_outer; i++)
+        {
+#pragma omp parallel num_threads(nest_threads)
+            {
+#pragma omp single
+                {
+#pragma omp task
+                    TopDownSplitMerge(0, vector_lengths[i], vector_vectors[i]);
+                }
+            }
+        }
 }
 
-void msort(int *v, long l) {
-    TopDownSplitMerge(0, l, v);
-}
+void msort(int *v, long l, short nest_threads) {
 
+}
 
 void TopDownSplitMerge(long first, long last, int *v) {
     if (last - first <= 1) {
@@ -36,19 +52,28 @@ void TopDownSplitMerge(long first, long last, int *v) {
 
     long mid = (last + first) / 2;
 
+#pragma omp task if(last-first > 500)
     TopDownSplitMerge(first, mid, v);
+#pragma omp task if(last-first > 500)
     TopDownSplitMerge(mid, last, v);
-    long i = first;
-    long j = mid;
-    for (long k = first; k < last; k++) {
-        if (i < mid && (j >= last || v[i] <= v[j])) {
-            v[k] = v[i];
-            i++;
-        } else {
-            v[k] = v[j];
-            j++;
+#pragma omp taskwait
+
+#pragma omp task if(last-first > 1000)
+    {
+        long i = first;
+        long j = mid;
+        for (long k = first; k < last; k++) {
+            if (i < mid && (j >= last || v[i] <= v[j])) {
+                v[k] = v[i];
+                i++;
+            } else {
+                v[k] = v[j];
+                j++;
+            }
         }
     }
+
+#pragma omp taskwait
 }
 
 
@@ -76,6 +101,7 @@ int main(int argc, char **argv) {
     Order order = ASCENDING;
     int length_inner_min = 100;
     int length_inner_max = 1000;
+    short outer_threads = 1;
 
     int **vector_vectors;
     int *vector_lengths;
@@ -112,6 +138,9 @@ int main(int argc, char **argv) {
                 break;
             case 'p':
                 num_threads = atoi(optarg);
+                break;
+            case 't':
+                outer_threads = (short) atoi(optarg);
                 break;
             case '?':
                 if (optopt == 'l' || optopt == 's') {
@@ -173,13 +202,14 @@ int main(int argc, char **argv) {
     clock_gettime(CLOCK_MONOTONIC, &before);
 
     /* Sort */
-    vecsort(vector_vectors, vector_lengths, length_outer);
+    vecsort(vector_vectors, vector_lengths, length_outer, outer_threads);
 
     clock_gettime(CLOCK_MONOTONIC, &after);
     double time = (double)(after.tv_sec - before.tv_sec) +
-              (double)(after.tv_nsec - before.tv_nsec) / 1e9;
+                  (double)(after.tv_nsec - before.tv_nsec) / 1e9;
 
     printf("Vecsort took: % .6e \n", time);
+
 
     if(debug) {
         print_v(vector_vectors, vector_lengths, length_outer);
