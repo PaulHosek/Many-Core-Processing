411452f3d9edaa43ba7c7d2a4a051af42e9f8e9b
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-02-27 16:17:06 +0100

    - fixed outer loop parallelism

--- assignment_2/vecsort/compare_runtime.sh
+++ assignment_2/vecsort/compare_runtime.sh
@@ -1,8 +1,9 @@
 rm data.csv
 module load prun
 
-directories=(sequential parallel_v1_onlynested parallel_v2_onlyouter parallel_v3_both parallel_v4_both_o3)
-repetitions=1
+#directories=(sequential parallel_v1_onlynested parallel_v2_onlyouter parallel_v3_both parallel_v4_both_o3)
+directories=(parallel_v2_onlyouter)
+repetitions=1 # 100
 
 if [ ! -f data.csv ]; then
   touch data.csv
