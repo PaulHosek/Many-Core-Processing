6742c06ce68ffbb3ad24dbb6d25382a8a9665b20
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-02-28 14:51:12 +0100

    - additional experiment: data vs task parallelism

--- assignment_2/vecsort/compare_speedup.py
+++ assignment_2/vecsort/compare_speedup.py
@@ -2,29 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib as mpl
+from scipy import stats
 
 pd.set_option('display.max_columns', None)
 plt.style.use('seaborn-v0_8-darkgrid')
 mpl.rcParams['font.size'] = 16
 
 # load data
-df = pd.read_csv("data/27_short_1_2.csv")
+df = pd.read_csv("data/28_100.csv")
 # convert to speedup
 seq_mean = df[df["version"] == "sequential"]["runtime"].mean()
 df["speedup"] = seq_mean / df["runtime"]
-
+print(df)
 # aggregate data for bar plot
 df = df.groupby("version").agg(["mean", "sem"])
-print(df)
+
 df.reset_index(inplace=True)
 df.columns = ['Implementation', 'runtime_mean', 'runtime_sem', 'speedup_mean', 'speedup_sem']
 
 # rename and order bars
-df = df.replace({"parallel_v1_onlynested": "nested",
-                 "parallel_v2_onlyouter": "outer",
+df = df.replace({"parallel_v1_onlynested": "task",
+                 "parallel_v2_onlyouter": "data",
                  "parallel_v3_both": "both",
                  "parallel_v4_both_o3": "both+O3", })
-category_order = ['sequential', 'nested', 'outer', 'both', 'both+O3']
+category_order = ['sequential', 'task', 'data', 'both', 'both+O3']
 df['Implementation'] = pd.Categorical(df['Implementation'], categories=category_order, ordered=True)
 df = df.sort_values('Implementation')
 
@@ -33,14 +34,18 @@ versions = df["Implementation"]
 means = df['speedup_mean'].values
 sems = df['speedup_sem'].values
 
-fig, ax = plt.subplots(figsize=(12, 6))
+fig, ax = plt.subplots(figsize=(9, 9))
 ax.bar(versions, means, yerr=1.96 * sems, capsize=5, color="#8FA993")
 ax.set_xticks(versions, labels=versions)
 ax.set_title("Vecsort speedup comparison", fontsize=16)
 ax.set_xlabel("Version", fontsize=14)
-ax.set_ylabel("Speedup", fontsize=14)
-ax.set_ylim(1)
-
+ax.set_ylabel("Speedup (to sequential)", fontsize=14)
+plt.ylim(0)
 plt.xticks(rotation=0)
 
 plt.savefig("vecsort_rel.png", dpi=300, bbox_inches="tight")
+
+
+tval,pval = stats.ttest_ind()
+print(stats.ttest_ind())
+
--- assignment_2/vecsort/parallel_v3_both/ot_compare_speedup.py
+++ assignment_2/vecsort/parallel_v3_both/ot_compare_speedup.py
@@ -8,31 +8,44 @@ plt.style.use('seaborn-v0_8-darkgrid')
 mpl.rcParams['font.size'] = 16
 
 # load data
-df = pd.read_csv("exp_outer_thread_old.csv")
-# convert to speedup
-seq_mean = df[df["nr_outer_threads"] == 1]["runtime"].mean()
+df = pd.read_csv("exp_outer_thread_28_2.csv")
+
+
+# convert to speedup to sequential
+df_seq = pd.read_csv("../data/28_100.csv")
+seq_mean = df_seq[df_seq["version"] == "sequential"]["runtime"].mean()
+print(seq_mean)
+
+
+
+
 df["speedup"] = seq_mean / df["runtime"]
+
+
+
 
 # aggregate data for bar plot
 df = df.groupby("nr_outer_threads").agg(["mean", "sem"])
-
+print(df)
 df.reset_index(inplace=True)
 
 df.columns = ['nr_outer_threads', 'runtime_mean', 'runtime_sem', 'speedup_mean', 'speedup_sem']
+print(df)
 
 x_vals = df["nr_outer_threads"]
-mean = df["runtime_mean"]
-upper = mean + 1.96 * df["runtime_sem"]
-lower = mean - 1.96 * df["runtime_sem"]
+mean = df["speedup_mean"]
+upper = mean + 1.96 * df["speedup_sem"]
+lower = mean - 1.96 * df["speedup_sem"]
 
-plt.figure()
+plt.figure(figsize=(8,8))
 plt.title("Outer loop threading speedup comparison")
 plt.fill_between(x_vals, upper, lower, alpha=0.5, color="#8FA993")
 
 plt.plot(x_vals, mean, color="#526F75", alpha=0.8)
 plt.scatter(x_vals, mean, color="#526F75", alpha=0.8, s=4)
-plt.xlabel("Nr of threads in the outer loop")
-plt.ylabel("Speedup (to single thread)")
+plt.xlabel("Nr threads assigned to data parallelism")
+plt.ylabel("Speedup (to sequential)")
 plt.xticks(np.arange(1, 17, 2))
+plt.ylim(0)
 
 plt.savefig("exp_outer_thread.png", dpi=300, bbox_inches="tight")
