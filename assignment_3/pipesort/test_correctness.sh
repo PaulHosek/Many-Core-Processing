4a156e9db7a42c44257311a59d5e45cc727c1167
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-03-10 12:53:12 +0100

    - testing bash confirms correct results

--- assignment_3/pipesort/test_correctness.sh
+++ assignment_3/pipesort/test_correctness.sh
@@ -2,9 +2,11 @@ module load prun
 
 make clean
 make
+nr_tests=10
+
 # sort 10 strings of numbers with different seeds and of length up to 4000
 # test if matches expected output
-for ((i=1;i<10;i++)); do
+for ((i=1;i<nr_tests;i++)); do
   sorted_numbers=$(prun -np 1 -v pipesort -l $((RANDOM % 4000 + 1)) -s $((RANDOM)) -p | grep -oP '\d+') #match any digits
   if [[ $(echo $sorted_numbers | tr ' ' '\n' | sort -n) == $(echo $sorted_numbers | tr ' ' '\n') ]]; then
     echo "Test passed: the output is sorted correctly."
