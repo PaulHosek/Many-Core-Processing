89b4116874618082b2e2d3267865e57f154a28e6
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-03-10 17:13:34 +0100

    - cleanup

--- assignment_3/pipesort/plot_buffersize.py
+++ assignment_3/pipesort/plot_buffersize.py
@@ -1,31 +1,35 @@
-from matplotlib import rcParams
+from matplotlib import rcParams, colors
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import seaborn as sns
 
 plt.style.use('seaborn-v0_8-darkgrid')
 rcParams['font.size'] = 16
 
-data = pd.read_csv("data/res_buffersize_2rep.csv")
+data = pd.read_csv("data/res_buffersize_20rep.csv")
 
 grouped = data.groupby(["n_nums", "buffersize"]).agg(["mean", "sem"])
 grouped.reset_index(inplace=True)
 grouped.columns = ["n_nums", "buffersize", "runtime_mean", "runtime_sem"]
 
-plt.figure()
+cmap = colors.LinearSegmentedColormap.from_list("mycmap", ["green", "red"])
+
+fig, ax = plt.subplots()
 lengths = sorted(list(set(grouped["n_nums"])))
-for i in lengths:
-    subset = grouped[grouped["n_nums"] == i]
-    cur_mean = subset["runtime_mean"]
-    plt.plot(subset["buffersize"], cur_mean, label=i)
+ax.set_prop_cycle(color=cmap(np.linspace(0, 1, len(lengths))))
+
+# for i in lengths:
+#     subset = grouped[grouped["n_nums"] == i]
+#     cur_mean = subset["runtime_mean"]
+#     plt.plot(subset["buffersize"], cur_mean, label=i)
 #     plt.fill_between(subset["buffersize"], cur_mean+1.96*cur_mean, cur_mean-1.96*cur_mean, alpha = 0.4)
-# ax = sns.lineplot(x='buffersize', y='runtime_mean',hue="n_nums", data=grouped)
+ax = sns.lineplot(x='buffersize', y='runtime_mean',hue="n_nums", data=grouped)
 # ax = sns.lineplot(x='buffersize', y='runtime_mean', data=grouped[grouped["n_nums"]==1000])
 
 plt.xlabel("Buffersize")
 plt.ylabel("Runtime (seconds)")
-plt.xlim((0,50))
-# plt.xscale("log")
+plt.xscale("log")
 # plt.yscale("log")
 plt.legend()
 plt.savefig("test.png", bbox_inches="tight", dpi=300)
--- assignment_3/pipesort/plot_longest_seq.py
+++ assignment_3/pipesort/plot_longest_seq.py
@@ -10,14 +10,25 @@ data = pd.read_csv("data/res_longest_seq.csv")
 
 
 print(data)
+
+# ax = sns.lineplot(x='n_nums', y='runtime', data=data[data["buffersize"]==10], label="10")
+# ax = sns.lineplot(x='n_nums', y='runtime', data=data[data["buffersize"]==100], label="100")
 plt.figure()
 
-ax = sns.lineplot(x='n_nums', y='runtime', data=data[data["buffersize"]==10], label="10")
-ax = sns.lineplot(x='n_nums', y='runtime', data=data[data["buffersize"]==100], label="100")
+sns.lineplot(x='n_nums', y='runtime', hue="buffersize", data=data, palette=["#809F92", "#446469"])
+
 
 plt.xlabel("Length random sequence")
 plt.ylabel("Runtime (seconds)")
+# # Add a black box around the legend
+legend = plt.legend(title="Buffer size")
+# frame = legend.get_frame()
+# frame.set_linewidth(0.0)
+# frame.set_facecolor('black')
+# frame.set_alpha(1)
+
+# Set the legend background color to white
+# legend.legendPatch.set_facecolor('white')
 # plt.xscale("log")
 # plt.yscale("log")
-plt.legend()
 plt.savefig("test.png", bbox_inches="tight", dpi=300)
