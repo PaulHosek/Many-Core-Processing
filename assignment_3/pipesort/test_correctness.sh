42cafa3407b7101a26dbb13da987bc3cefdd3a37
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-03-10 16:00:38 +0100

    - cleanup

--- assignment_3/pipesort/test_correctness.sh
+++ assignment_3/pipesort/test_correctness.sh
@@ -16,11 +16,13 @@ last_number=-1
 # sort 10 strings of numbers with different seeds and of length up to 4000
 # test if matches expected output
 for ((i=1;i<nr_tests;i++)); do
+  last_number=-1
   myrand=$((RANDOM % 4000 + 1))
   myrand2=$((RANDOM % 1000 + 1))
   seed=$((RANDOM % 10000 + 1))
   sorted_numbers=$(prun -np 1 -v pipesort -l $myrand -s $seed -p -b $myrand | grep -oP '\d+') #match any digits
-  last_number=-1
+
+  # test if is sorted
   for num in $sorted_numbers; do
     if (( $num < $last_number )); then
       is_sorted=false
@@ -30,13 +32,13 @@ for ((i=1;i<nr_tests;i++)); do
     last_number=$num
   done
 
-
+  # return
   if $is_sorted; then
     echo -e "Length of sequence was ${BLUE} $myrand ${NC}, with bb size ${BLUE}$myrand2${NC}, seed=${BLUE}$seed${NC}"
-    echo -e "Test passed: the output is sorted ${GREEN}correctly.${NC}"
+    echo -e "${GREEN}Passed.${NC}"
   else
     echo -e "Length of sequence was ${BLUE} $myrand ${NC}, with bb size ${BLUE}$myrand2${NC}, seed=${BLUE}$seed${NC}"
-    echo -e "${RED} Test failed: the output is not sorted correctly.${NC}"
+    echo -e "${RED}Failed${NC}"
     echo "$sorted_numbers"
     break
   fi
