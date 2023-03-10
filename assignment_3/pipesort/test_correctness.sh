38c8505f017eb8e64d38b57405b5e1cb3eaea6a9
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-03-10 15:58:14 +0100

    - ok pipesort works

--- assignment_3/pipesort/test_correctness.sh
+++ assignment_3/pipesort/test_correctness.sh
@@ -10,24 +10,26 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 is_sorted=true
-last_number=999999999999999999999
+last_number=-1
+
 
 # sort 10 strings of numbers with different seeds and of length up to 4000
 # test if matches expected output
 for ((i=1;i<nr_tests;i++)); do
-  myrand=$((RANDOM % 10 + 1))
+  myrand=$((RANDOM % 4000 + 1))
   myrand2=$((RANDOM % 1000 + 1))
   seed=$((RANDOM % 10000 + 1))
   sorted_numbers=$(prun -np 1 -v pipesort -l $myrand -s $seed -p -b $myrand | grep -oP '\d+') #match any digits
-
+  last_number=-1
   for num in $sorted_numbers; do
-    if (( $last_number <= $num )); then
+    if (( $num < $last_number )); then
       is_sorted=false
-      echo $num $last_number
+      echo "not $last_number<=$num"
       break
     fi
     last_number=$num
   done
+
 
   if $is_sorted; then
     echo -e "Length of sequence was ${BLUE} $myrand ${NC}, with bb size ${BLUE}$myrand2${NC}, seed=${BLUE}$seed${NC}"
