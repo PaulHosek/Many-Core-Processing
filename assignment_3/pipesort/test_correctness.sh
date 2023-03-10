51703008a4035c28f22bdf0fc2ccab6401a8528a
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-03-10 15:50:03 +0100

    - more tests, pipesort still wong

--- assignment_3/pipesort/test_correctness.sh
+++ assignment_3/pipesort/test_correctness.sh
@@ -10,19 +10,20 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 is_sorted=true
-last_number=-999999
+last_number=999999999999999999999
 
 # sort 10 strings of numbers with different seeds and of length up to 4000
 # test if matches expected output
 for ((i=1;i<nr_tests;i++)); do
-  myrand=$((RANDOM % 100 + 1))
+  myrand=$((RANDOM % 10 + 1))
   myrand2=$((RANDOM % 1000 + 1))
   seed=$((RANDOM % 10000 + 1))
   sorted_numbers=$(prun -np 1 -v pipesort -l $myrand -s $seed -p -b $myrand | grep -oP '\d+') #match any digits
 
   for num in $sorted_numbers; do
-    if (( $num <= $last_number )); then
+    if (( $last_number <= $num )); then
       is_sorted=false
+      echo $num $last_number
       break
     fi
     last_number=$num
@@ -34,7 +35,7 @@ for ((i=1;i<nr_tests;i++)); do
   else
     echo -e "Length of sequence was ${BLUE} $myrand ${NC}, with bb size ${BLUE}$myrand2${NC}, seed=${BLUE}$seed${NC}"
     echo -e "${RED} Test failed: the output is not sorted correctly.${NC}"
-    echo sorted_numbers
+    echo "$sorted_numbers"
     break
   fi
 done
