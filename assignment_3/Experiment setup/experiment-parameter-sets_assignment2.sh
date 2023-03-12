c011544cbc6154ba6ef62350a486df7d12b614e8
Denniscrevus <denniscurti@yahoo.com>
2023-03-12 15:05:53 +0100

    Added experiment setup

---
+++ assignment_3/Experiment setup/experiment-parameter-sets_assignment2.sh
@@ -0,0 +1,23 @@
+make clean
+make heat_omp
+
+maxiter=100
+e=0.0001
+L=0
+H=100
+data="data_assignment2.csv"
+repetitions=10
+start=1
+experiment_dir=../../test/experiments_assignment2/*
+END=25
+
+for m in ${experiment_dir}; do
+  for ((it=${start}; it<=${repetitions}; it++)); do
+    for i in $(seq 1 ${END}); do
+      #Sequential
+      array=($(sed -n '2p' ${m}));
+      echo -n "${array[0]}, ${array[1]}, ${i} " >> ${data};
+      prun -np 1 -v heat_omp -n ${array[1]} -m ${array[0]} -i ${maxiter} -e ${e} -c ${m} -t ${m} -r 1 -k ${maxiter} -L ${L} -H ${H} -p ${i};
+    done
+  done
+done
